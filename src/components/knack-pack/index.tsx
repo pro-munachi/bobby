22125f6d47d43b962104e4f9c3a9c5eab29f7ebc
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-12-11 12:16:48 +0000

    create assests and data directories

--- src/components/knack-pack/index.tsx
+++ src/components/knack-pack/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
+import { ReactComponent as IconLogo } from "../../logo.svg";
 
 const KnackPack: React.FC = () => {
-  return <div>KnackPack</div>;
+  return <IconLogo />;
 };
 
 export default KnackPack;
