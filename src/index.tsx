661576ed97bb9ac7103054105125c40dc6201082
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-12-17 15:34:09 +0000

    install reselect

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store, persistor } from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/* <Router> */}
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+    {/* </Router> */}
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
